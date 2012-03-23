892fed1de8583b4a5125107d48d6a056340a12a0
Bryan Helmig <bryan@bryanhelmig.com>
2012-03-23 17:41:28 -0500

    add ace

---
+++ pythonpie/src/coffee/main.coffee
@@ -0,0 +1,16 @@
+$(document).ready ->
+  editor = ace.edit 'editor'
+  editor.setTheme 'ace/theme/twilight'
+  PythonMode = require('ace/mode/python').Mode
+  editor.getSession().setMode new PythonMode()
+
+  $('.btn-submit').click ->
+    code = editor.getSession().getValue()
+    $.ajax
+      type: 'POST'
+      url: '/v1/python/2.7.1'
+      data: JSON.stringify {code: code}
+      contentType: "application/json; charset=utf-8",
+      success: (data, textStatus, jqXHR) ->
+        console.log data, textStatus, jqXHR
+        $('.results').text data.results
---
+++ pythonpie/static/js/main.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+  var PythonMode, editor;
+  editor = ace.edit('editor');
+  editor.setTheme('ace/theme/twilight');
+  PythonMode = require('ace/mode/python').Mode;
+  editor.getSession().setMode(new PythonMode());
+  return $('.btn-submit').click(function() {
+    var code;
+    code = editor.getSession().getValue();
+    return $.ajax({
+      type: 'POST',
+      url: '/v1/python/2.7.1',
+      data: JSON.stringify({
+        code: code
+      }),
+      contentType: "application/json; charset=utf-8",
+      success: function(data, textStatus, jqXHR) {
+        console.log(data, textStatus, jqXHR);
+        return $('.results').text(data.results);
+      }
+    });
+  });
+});
